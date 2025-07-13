5fa14c132aace6f49b4525b6d1fbffd484d84ab7
PRASHANT TIWARI <prashant0003tiwari@gmail.com>
2025-07-13 23:26:30 +0530

    TopKFrequentElements.java

---
+++ DSA/35. Top K Frequent Elements/TopKFrequentElements.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class TopKFrequentElements {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+
+        List<Integer> result = topKFrequent(nums, k);
+        
+        System.out.println(result);
+    }
+
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(
+            (a, b) -> a.getValue() - b.getValue()
+        );
+
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            minHeap.offer(entry);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        List<Integer> topKElements = new ArrayList<>();
+        while (!minHeap.isEmpty()) {
+            topKElements.add(minHeap.poll().getKey());
+        }
+
+        return topKElements;
+    }
+}
