6083594246391c085d1e9745f0e8ade9d30ee6f1
PRASHANT TIWARI <prashant0003tiwari@gmail.com>
2025-07-25 22:57:28 +0530

    StockSpanner.java

---
+++ DSA/47. Online Stock Span/StockSpanner.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+class StockSpanner {
+    private Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1; 
+        
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1]; 
+        }
+        
+        stack.push(new int[]{price, span});
+        
+        return span; 
+    }
+
+    public static void main(String[] args) {
+        StockSpanner stockSpanner = new StockSpanner();
+        System.out.println(stockSpanner.next(100)); // Output: 1
+        System.out.println(stockSpanner.next(80));  // Output: 1
+        System.out.println(stockSpanner.next(60));  // Output: 1
+        System.out.println(stockSpanner.next(70));  // Output: 2
+        System.out.println(stockSpanner.next(60));  // Output: 1
+        System.out.println(stockSpanner.next(75));  // Output: 4
+        System.out.println(stockSpanner.next(85));  // Output: 6
+    }
+}
