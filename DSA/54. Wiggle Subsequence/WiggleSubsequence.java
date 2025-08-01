acf8e07fd030db9ef3c4efb98d83abecf49691cc
PRASHANT TIWARI <prashant0003tiwari@gmail.com>
2025-08-01 23:16:26 +0530

    WiggleSubsequence.java

---
+++ DSA/54. Wiggle Subsequence/WiggleSubsequence.java
@@ -0,0 +1,32 @@
+public class WiggleSubsequence {
+    public int wiggleMaxLength(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        
+        int up = 1;
+        int down = 1; 
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            } else if (nums[i] < nums[i - 1]) {
+                down = up + 1; 
+            }
+        
+        }
+        
+        return Math.max(up, down);
+    }
+
+    public static void main(String[] args) {
+        WiggleSubsequence ws = new WiggleSubsequence();
+        int[] nums = {1, 7, 4, 9, 2, 5};
+        System.out.println("Length of the longest wiggle subsequence: " + ws.wiggleMaxLength(nums));
+    }
+}
